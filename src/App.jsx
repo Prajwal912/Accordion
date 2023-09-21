d9677dc9ec092a125b5a0f95fb6307807ed7252e
prajwal <prajwalshah912@gmail.com>
2023-09-22 00:20:56 +0530

    body styling setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import AccordionMenu from "./components/AccordionMenu";
 const App = () => {
   return (
     <>
-     
+      <div className="flex flex-col justify-center items-center	 h-screen m-0 p-0" style={{'fontFamily': 'roboto'}}>
+        <AccordionMenu />
+      </div>
     </>
   );
 };
