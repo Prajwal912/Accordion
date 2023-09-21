f3c3b0b23a09e35133b9f8fd8d2548d75e00f74d
prajwal <prajwalshah912@gmail.com>
2023-09-22 00:25:03 +0530

    basic component cleanup

--- src/components/AccordionMenu.jsx
+++ src/components/AccordionMenu.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import AccordionItem from './AccordionItem';
 
 const AccodionMenu = () => {
   return (
+    <>
     <div>AccodionMenu</div>
+   <AccordionItem />
+    </>
   )
 }
 
