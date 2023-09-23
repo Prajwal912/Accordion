75e314a46b9d29aa864bca14e057f47ea5e77fcf
prajwal <prajwalshah912@gmail.com>
2023-09-23 21:46:07 +0530

    All wrapped and done

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import '../styles/Accordion.css';
+import "../styles/Accordion.css";
 
-const AccordionItem = ({title, content}) => {
+const AccordionItem = ({ title, content, isActive, onClick }) => {
   return (
     <>
-      <div className="accordion--item active
-       flex flex-col items-start w-[600px] p-5
-        rounded-[15px] pl-[72px] cursor-pointer">
-        <div className="icon--container active
-        ">
-          <div className="stroke--one active"></div>
+      <div
+        className={`accordion--item ${
+          isActive ? "active" : ""
+        } flex flex-col items-start w-[600px] p-5
+        rounded-[15px] pl-[72px] cursor-pointer`}
+        onClick={onClick}
+      >
+        <div className={`icon--container ${isActive ? "active" : ""}`}>
+          <div className={`stroke--one ${isActive ? "active" : ""}`}></div>
           <div className="stroke--two"></div>
         </div>
-        <h1 className="accordion--title 
-         text-black	text-base font-medium">{title}</h1>
-        <p className="accordion--content active
-        max-h-0 text-black m-0 font-light overflow-hidden text-xs	">
+        <h1
+          className="accordion--title 
+         text-black	text-base font-medium"
+        >
+          {title}
+        </h1>
+        <p
+          className={`accordion--content ${
+            isActive ? "active" : ""
+          } max-h-0 text-black m-0 font-light overflow-hidden text-xs`}
+          onClick={onClick}
+        >
           {content}
         </p>
       </div>
--- src/components/AccordionMenu.jsx
+++ src/components/AccordionMenu.jsx
@@ -1,7 +1,13 @@
   import React from "react";
 import AccordionItem from "./AccordionItem";
+import { useState } from "react";
 
 const AccodionMenu = () => {
+
+  const [currentlyActive, setcurrentlyActive] = useState(1)
+  const handleAccordionClick = (newActiveAccordion) => {
+    setcurrentlyActive(newActiveAccordion)
+  }
   return (
     <>
       <div className="accordion--menu  flex flex-col justify-center items-center py-[50px] px-[30px] rounded-2xl border border-gray-300 shadow-md w-auto">
@@ -13,18 +19,26 @@ const AccodionMenu = () => {
         <AccordionItem
           title="Accordion #1"
           content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. first!!"
+          isActive={currentlyActive === 1}
+          onClick={() => handleAccordionClick(1)}
         />
         <AccordionItem
           title="Accordion #2"
           content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. second!!"
+            isActive={currentlyActive === 2}
+          onClick={() => handleAccordionClick(2)}
         />
         <AccordionItem
           title="Accordion #3"
           content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. third!!"
+            isActive={currentlyActive === 3}
+          onClick={() => handleAccordionClick(3)}
         />
         <AccordionItem
           title="Accordion #4"
           content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. fourth!!"
+            isActive={currentlyActive === 4}
+          onClick={() => handleAccordionClick(4)}
         />
 
           
